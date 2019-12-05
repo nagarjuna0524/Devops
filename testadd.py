c3c8e162369643ebfbf12e7cec6a82e0736bfe32
Devops <nagarjuna.palagiri@gmail.com>
2019-12-05 14:30:55 +0000

    testadd1

--- testadd.py
+++ testadd.py
@@ -1,5 +1,7 @@
-n1 = input('Enter number:')
-n2 = input('Enter number2:')
+#!/usr/bin/python3
+import sys
+n1 = sys.argv[1]
+n2 = sys.argv[2]
 sum = float(n1) + float(n2)
 print('The sum of {0} and {1} is {2}'.format(n1,n2,sum))
 print('Hello Python World')
