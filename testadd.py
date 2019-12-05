005fc109dbe22cbf5dfaf6ec76270131bcdf8dd1
Devops <nagarjuna.palagiri@gmail.com>
2019-12-05 14:36:02 +0000

    test2

--- testadd.py
+++ testadd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
-n1 = sys.argv[1]
-n2 = sys.argv[2]
-sum = float(n1) + float(n2)
-print('The sum of {0} and {1} is {2}'.format(n1,n2,sum))
+a=sys.argv[1:]
+b=sys.argv[2:]
+sumn=str(a+b)
+print(" sum is",sumn)
 print('Hello Python World')
